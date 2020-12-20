036762a53ff21676eb398a8b56c812dbb43c550a
Saif Bechan <saifbechan@gmail.com>
2020-12-20 21:19:52 +0100

    Hot pink stroke for title & adjusted hitbox

--- src/Entities/Obstacles/Title.ts
+++ src/Entities/Obstacles/Title.ts
@@ -9,13 +9,14 @@ export default class Title implements Obstacle {
   private readonly pos: Vector;
 
   private readonly image: Image;
+  private readonly scale;
   private readonly height;
   private width = 0;
 
   constructor({ p5, image }: ObstacleProps) {
     this.p5 = p5;
     this.image = image || p5.createImage(1, 1);
-    this.pos = p5.createVector(p5.width / 2, 300);
+    this.pos = p5.createVector(p5.width / 2, 400);
 
     switch (getViewport(p5.width)) {
       case Viewport.XS:
@@ -30,6 +31,8 @@ export default class Title implements Obstacle {
       default:
         this.height = 78;
     }
+
+    this.scale = (this.height / 5) * 4;
   }
 
   gotHit(rocket: Rocket): boolean {
@@ -37,7 +40,7 @@ export default class Title implements Obstacle {
     return (
       rocketPosition.x > this.pos.x - this.width / 2 &&
       rocketPosition.x < this.pos.x + this.width / 2 &&
-      rocketPosition.y > this.pos.y - 10 &&
+      rocketPosition.y > this.pos.y - this.scale &&
       rocketPosition.y < this.pos.y + 10
     );
   }
@@ -45,22 +48,24 @@ export default class Title implements Obstacle {
   render(): void {
     const title = 'rocketeers';
 
+    this.p5.stroke(255, 20, 147, 80);
+    this.p5.strokeWeight(2);
     this.p5.textFont('Bungee Outline');
     this.p5.textAlign(this.p5.CENTER);
     this.p5.textSize(this.height);
     this.p5.fill(255);
     this.p5.text(title, this.pos.x, this.pos.y);
+    this.p5.noStroke();
 
     this.width = Math.floor(this.p5.textWidth(title));
 
-    const scale = (this.height / 5) * 4;
     this.p5.imageMode(this.p5.CENTER);
     this.p5.image(
       this.image,
       this.pos.x - this.width / 1.9,
-      this.pos.y - scale,
-      scale,
-      scale
+      this.pos.y - this.scale,
+      this.scale,
+      this.scale
     );
   }
 }
