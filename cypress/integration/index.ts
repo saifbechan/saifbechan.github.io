7d9b46f1f2d7f8f2420688061592f906e169eddf
Saif Bechan <saifbechan@gmail.com>
2021-02-18 23:40:20 +0100

    cypress test to see if there are no errors in the console

--- cypress/integration/index.ts
+++ cypress/integration/index.ts
@@ -1,3 +1,11 @@
+import Agent = Cypress.Agent;
+import Sinon from 'cypress/types/sinon';
+
+let spy: Agent<Sinon.SinonSpy>;
+Cypress.on('window:before:load', (win) => {
+  spy = cy.spy(win.console, 'error');
+});
+
 describe('index', () => {
   it('displays the footer', () => {
     cy.visit('/');
@@ -7,6 +15,18 @@ describe('index', () => {
   it('displays a p5 canvas', () => {
     cy.visit('/');
     cy.get('.p5Canvas').should('be.visible');
+  });
+
+  it('does not log any errors', () => {
+    cy.wait(100).then(() => {
+      expect(spy).not.to.be.called;
+    });
+
+    cy.wrap({}).should(() => {
+      expect(spy).not.to.be.called;
+    });
+
+    expect(spy).not.to.be.called;
   });
 });
 
