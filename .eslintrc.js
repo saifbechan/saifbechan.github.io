e4c0ca922b4ca96cd5345721cd3dbf0135b902e2
Saif Bechan <saifbechan@gmail.com>
2021-02-24 22:34:20 +0100

    bumped deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
-        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
       ],
       rules: {
