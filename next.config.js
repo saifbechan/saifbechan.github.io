e7af599060352ddd48efd3f4bf1e5941d5120c52
Saif Bechan <saifbechan@gmail.com>
2022-06-27 16:57:11 +0200

    chore: nextjs improvements

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withOffline = require('next-offline');
+const withPWA = require('next-pwa');
 
-const nextConfig = {
-  target: 'serverless',
-};
-
-module.exports = withOffline(nextConfig);
+module.exports = withPWA({
+  swcMinify: true,
+  pwa: {
+    dest: 'public',
+  },
+});
